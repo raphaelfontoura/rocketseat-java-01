91c454d5c94181e15a544998ececb94bb56e4112
Raphael Fontoura <raphael.fontoura@gmail.com>
2023-10-13 10:50:32 -0300

    Apaga imports não utilizados.

--- src/main/java/com/github/raphaelfontoura/todolist/task/TaskController.java
+++ src/main/java/com/github/raphaelfontoura/todolist/task/TaskController.java
@@ -1,13 +1,11 @@
 package com.github.raphaelfontoura.todolist.task;
 
-import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.github.raphaelfontoura.todolist.errors.ErrorModel;
 import com.github.raphaelfontoura.todolist.utils.BeanUpdateUtil;
 
 import jakarta.persistence.EntityNotFoundException;
--- src/main/java/com/github/raphaelfontoura/todolist/user/UserController.java
+++ src/main/java/com/github/raphaelfontoura/todolist/user/UserController.java
@@ -1,17 +1,11 @@
 package com.github.raphaelfontoura.todolist.user;
 
-import java.time.Instant;
-
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
-
-import com.github.raphaelfontoura.todolist.errors.ErrorModel;
 
 import lombok.RequiredArgsConstructor;
 
